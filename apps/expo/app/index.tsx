cb64eaf58e5471da031c54e88b060e1effa07a9d
SSOURABH58 <ssourabh025@gamil.com>
2023-07-04 16:32:55 +0530

    permication issue

--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,3 +1,5 @@
 import { HomeScreen } from 'app/features/home/screen'
+import AudioScreen from 'app/features/audio/screen'
 
-export default HomeScreen
+// export default HomeScreen
+export default AudioScreen
